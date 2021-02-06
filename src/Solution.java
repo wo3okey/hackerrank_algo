e20fa2daff6e9a9e8ee01f96cddd3412c3970d8f
we-chall <wo3okey@naver.com>
2021-02-07 01:46:40 +0900

    BalancedBrackets

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,42 @@
-public class Solution {
-    public static void main(String agrs[]) {
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
+public class Solution {
+    static String isBalanced(String s) {
+        List<Character> opens = Arrays.asList('{', '[', '(');
+        List<Character> closes = Arrays.asList('}', ']', ')');
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            // 닫히는 기호면 스택에서 꺼내면서 짝 맞는지 확인
+            if (closes.contains(c)) {
+                if (stack.isEmpty()) {
+                    return "NO";
+                }
+
+                char closeChar = stack.pop();
+                if (!(closeChar == '{' && c == '}') && !(closeChar == '[' && c == ']') && !(closeChar == '(' && c == ')')) {
+                    return "NO";
+                }
+            }
+
+            // 열리는 기호면 스택에 넣기
+            if (opens.contains(c)) {
+                stack.push(c);
+            }
+        }
+        if (stack.size() > 0) {
+            return "NO";
+        }
+        return "YES";
+    }
+
+
+    public static void main(String agrs[]) {
+        String s = "[]([{][][)(])}()([}[}(})}])}))]](}{}})[]({{}}))[])(}}[[{]{}]()[(][])}({]{}[[))[[}[}{(]})()){{(]]){][";
+//        String s = "{{[[(())]]}}";
+        String result = isBalanced(s);
+        System.out.print(result);
     }
 }
 
